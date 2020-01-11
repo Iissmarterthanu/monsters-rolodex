3b294e730d24f4008cf17f9b680e92b5e8603d33
FD <iissmarterthanu@yahooo.com>
2020-01-11 16:26:09 +0300

    adding files for github pages 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'the ruff gang',
+      string: 'the Ruff Gang',
       monsters: [], //comment
       searchField: ''
     };
