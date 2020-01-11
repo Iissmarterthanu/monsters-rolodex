188a9de445e0aa205a5da9c836017046cb8f5392
FD <iissmarterthanu@yahooo.com>
2020-01-11 15:58:38 +0300

    Updates

---
+++ precache-manifest.81971d978b9b70104ba5a324d260cc44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1229c782f846dbf6511091a6f61ca3ae",
+    "url": "/iissmarterthanu.github.io/monsters-rollodex/index.html"
+  },
+  {
+    "revision": "52243ca4ec40c5b4d2ca",
+    "url": "/iissmarterthanu.github.io/monsters-rollodex/static/css/main.5b84d308.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/iissmarterthanu.github.io/monsters-rollodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/iissmarterthanu.github.io/monsters-rollodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52243ca4ec40c5b4d2ca",
+    "url": "/iissmarterthanu.github.io/monsters-rollodex/static/js/main.8a2cc108.chunk.js"
+  },
+  {
+    "revision": "9dd27ec51e299449dbaa",
+    "url": "/iissmarterthanu.github.io/monsters-rollodex/static/js/runtime-main.a5bf9ade.js"
+  }
+]);
